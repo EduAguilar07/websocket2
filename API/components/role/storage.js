5c62cb7bf98ff3515f391d2523d8d0c99de444cc
e.aguilar <e.aguilar@jardinazuayo.fin.ec>
2023-10-28 12:17:08 -0500

    roles

---
+++ API/components/role/storage.js
@@ -0,0 +1,23 @@
+const Model = require('./model')
+const ModelRol = require('../role/model')
+
+async function agregarRol( dato ) {
+    const resultado = await new Model( dato )
+    const rol = resultado.save()
+    return rol
+}
+
+async function obtenerRol( filtro ) {
+    let mi_filtro = {}
+
+    if (filtro.name != null) {
+        mi_filtro = { name: filtro.name }
+    }
+    const resultado = await Model.find( mi_filtro )
+    return resultado
+}
+
+module.exports = {
+    agregar:agregarRol,
+    obtener:obtenerRol
+}
